5d7d1db19c28092dcf8b87ec670689224d5f7589
Vamshedhar Reddy <vamshedhar@gmail.com>
2017-04-28 14:05:27 -0400

    init(): initial db connect

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+  include("dbconnect.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Beer Reviews</title>
+</head>
+<body>
+  <h1>Hello world</h1>
+</body>
+</html>
